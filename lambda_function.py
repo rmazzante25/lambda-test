3e8ca0f717a9e61d2b606557823a34cd87764a01
Reynaldo <rmazzante25@gmail.com>
2023-08-26 17:05:12 -0300

    teste pipeline 5

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,6 @@
-import pandas as pd
-
-def index(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.1')
+def handler(event, context):
+    return {
+    'statusCode': 200,
+    'body': 'Hello from Lambda to Reynaldo!'
+}
+       
